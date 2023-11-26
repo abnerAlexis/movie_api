0df9732cf1242737fc6b1226cd01e693fed31cf3
Alexis Abner <alexisabner@alexiss-air.lan>
2023-11-26 14:58:47 -0700

    Create write streamto add to a created txt file, update documentation.html

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express'),
-  morgan = require('morgan');
+  morgan = require('morgan'),
+  //Importing built-in node modules fs and path.
+  fs = require('fs'),
+  path = require('path');
 
 const app = express();
 
-app.use(morgan('common'));
+//creating a write stream in append mode, and a txt file in root dir.
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' });
+
+//setting up the logger
+app.use(morgan('combined', { stream: accessLogStream }));
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Welcome to the movie app!');
 });
 
-app.get('/secreturl', (req, res) => {
+http://localhost:8080/public/documentation.html
+app.get('/document', (req, res) => {
   res.send('This is a secret url with super top-secret content.');
 });
 
