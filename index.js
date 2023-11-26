39b184b7106a8328efa6a1a633316b6b9f96429e
Alexis Abner <alexisabner@alexiss-air.lan>
2023-11-26 12:41:55 -0700

    Removed index.html, remove the old log.txt,  set up morgan

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
-const express = require('express');
+const express = require('express'),
+  morgan = require('morgan');
+
 const app = express();
 
-let topBooks = [
-  {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
-    author: 'J.K. Rowling'
-  },
-  {
-    title: 'Lord of the Rings',
-    author: 'J.R.R. Tolkien'
-  },
-  {
-    title: 'Twilight',
-    author: 'Stephanie Meyer'
-  }
-];
+app.use(morgan('common'));
 
-// GET requests
 app.get('/', (req, res) => {
-  res.send('Welcome to my book club!');
+  res.send('Welcome to the movie app!');
 });
 
-app.get('/documentation', (req, res) => {                  
-  res.sendFile('public/documentation.html', { root: __dirname });
+app.get('/secreturl', (req, res) => {
+  res.send('This is a secret url with super top-secret content.');
 });
 
-app.get('/books', (req, res) => {
-  res.json(topBooks);
-});
-
-
-// listen for requests
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
 });
